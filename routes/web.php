76c90f82186e2caee77884835e5df96ee2813d77
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-23 23:32:46 +0100

    testing testing

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Home::class);
+Route::get('/', function(){
+    return "sqd";
+});
 
 Route::get('/login', function () {
     return view('auth/login');;
