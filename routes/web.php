c41d3899188b28a38da6432b76517fd6591f5e3f
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-24 03:57:00 +0100

    testing middleware

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,20 @@ Route::get('/', function(){
     return view('landing');;
 });
 
+Route::group(['middleware' => config('fortify.middleware', ['web'])], function () {
+
+    // with fortify guest middleware
+    Route::get('/login', function () {
+       return 'Foo';
+    })->middleware(['guest']);
+
+    // with fortify auth middleware
+    Route::get('/regiser', function () {
+       return 'bar';
+    }) ->middleware(['guest']); // fortify auth middleware
+
+});
+
 Route::get('/home', function(){
     dd(\Illuminate\Support\Facades\Auth::user());
 });
