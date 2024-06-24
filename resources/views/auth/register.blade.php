4032f1736d1614b58c622071bdb396cb1bfcfc53
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-25 00:18:52 +0100

    testing register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,36 @@
-<x-authlayout>
-    <div>
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-            <input type="text" placeholder="username" name="name" />
-            <input type="email" placeholder="email" name="email" />
-            <input type="password" placeholder="passowrd" name="password" />
-            <input type="password" placeholder="passowrd" name="password_confirmation" />
-            <button type="submit">submit</button>
-             @error('name')
-                 error
-             @enderror
-        </form>
+<x-layout>
+<form class="form" action="{{ route('register') }}" method="post">
+    <h2>Register</h2>
+    @csrf
+    <div class="form-group">
+        <input type="text" name="name" id="name" placeholder="Name">
+        @error("name")
+        <div class="error">{{ $message }}</div>
+        @enderror
     </div>
-</x-authlayout>
+    <div class="form-group">
+        <input type="email" name="email" id="email" placeholder="Email">
+        @error("email")
+        <div class="error">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <input type="tel" name="phone" id="phone" placeholder="Phone">
+        @error("phone")
+        <div class="error">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <input type="password" name="password" id="password" placeholder="Password">
+        @error("password")
+        <div class="error">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <button type="submit">Register</button>
+    </div>
+    <div class="form-group">
+        <p>Already have an account? <a href="#">Login</a></p>
+    </div>
+</form>
+</x-layout>
