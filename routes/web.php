4a62d8d6009942728295df95b26370ebc8049f63
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-24 19:49:27 +0100

    ooof

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,18 @@ Route::get('/', function(){
     return view('landing');;
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
- })->name('login');
-
-
- Route::get('/register', function () {
-    return view('auth.register');
- })->name('register');
-
 
 Route::group(['middleware' => config('fortify.middleware', ['web'])], function () {
+
+    Route::get('/login', function () {
+        return view('auth.login');
+     })->name('login');
+
+
+     Route::get('/register', function () {
+        return view('auth.register');
+     })->name('register');
+
 
     Route::get('/home', function(){
         dd(\Illuminate\Support\Facades\Auth::user());
