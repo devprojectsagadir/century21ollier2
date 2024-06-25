3b3d50fd33a5fd880f5128c933a33ffc66d73556
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-25 01:38:22 +0100

    updating login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,24 @@
 <x-authlayout>
-    <div>
-        <form action="">
-            <input type="text" />
-            <input type="password" />
-        </form>
-    </div>
+    <form class="form" action="{{ route('login') }}" method="post">
+        <h2>Login</h2>
+        @csrf
+        <div class="form-group">
+            <input type="email" name="email" id="email" placeholder="Email">
+            @error("email")
+             <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <input type="password" name="password" id="password" placeholder="Password">
+            @error("password")
+             <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <button type="submit">Login</button>
+        </div>
+        <div class="form-group">
+            <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+    </form>
 </x-authlayout>
